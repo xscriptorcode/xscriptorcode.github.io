23db3ecc38b1f4d3fdaeaa16a90525b93e8ebd1f
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-06-27 22:47:12 +0200

    update contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -18,8 +18,8 @@ export default function Contact() {
   return (
     <div>
       <CustomRippleCursor />
-      <main className={`${PagesStyles.mainContainer} fadeInUp`}>
-        <div className="flex-1 relative p-6 lg:pl-12 shadow-md min-h-[224px] flex flex-col justify-start">
+      <main className={`${PagesStyles.mainContainer} min-h-0`}>
+        <div className="flex-1 relative p-6 lg:pl-12 min-h-[224px] flex flex-col justify-start">
           <h1 className={`${PagesStyles.titles} ${Roboto_font.className}`}>
             <DecryptedText
               text="Contact"
@@ -48,14 +48,50 @@ export default function Contact() {
             />
           </h2>
         </div>
-        <div className="flex flex-row items-center justify-center gap-4 -mt-2">
-          <a href="https://t.me/xscriptor" target="_blank"><TelegramIcon /></a>
-          <a href="https://instagram.com/xscriptorcode" target="_blank"><InstagramIcon /></a>
-          <a href="https://wa.me/34666938748?text=Hello!" target="_blank"><WhatsappIcon /></a>
-          <a href="https://instagram.com/xscriptor" target="_blank"><InstagramIcon /></a>
-          <a href="https://github.com/xscriptorcode" target="_blank"><GithubIcon /></a>
+        <div className="flex flex-col items-center gap-4 mt-6">
+          <a
+            href="https://t.me/xscriptor"
+            target="_blank"
+            className="flex items-center justify-center gap-2 w-60 px-4 py-2 text-white bg-white/10 border border-white/20 backdrop-blur-lg rounded-full shadow-md transition-colors duration-300 hover:text-yellow-400"
+          >
+            Telegram <TelegramIcon />
+          </a>
+
+          <a
+            href="https://instagram.com/xscriptorcode"
+            target="_blank"
+            className="flex items-center justify-center gap-2 w-60 px-4 py-2 text-white bg-white/10 border border-white/20 backdrop-blur-lg rounded-full shadow-md transition-colors duration-300 hover:text-yellow-400"
+          >
+            Instagram (Xcode) <InstagramIcon />
+          </a>
+
+          <a
+            href="https://wa.me/34666938748?text=Hello!"
+            target="_blank"
+            className="flex items-center justify-center gap-2 w-60 px-4 py-2 text-white bg-white/10 border border-white/20 backdrop-blur-lg rounded-full shadow-md transition-colors duration-300 hover:text-yellow-400"
+          >
+            WhatsApp <WhatsappIcon />
+          </a>
+
+          <a
+            href="https://instagram.com/xscriptor"
+            target="_blank"
+            className="flex items-center justify-center gap-2 w-60 px-4 py-2 text-white bg-white/10 border border-white/20 backdrop-blur-lg rounded-full shadow-md transition-colors duration-300 hover:text-yellow-400"
+          >
+            Instagram (Xscriptor) <InstagramIcon />
+          </a>
+
+          <a
+            href="https://github.com/xscriptorcode"
+            target="_blank"
+            className="flex items-center justify-center gap-2 w-60 px-4 py-2 text-white bg-white/10 border border-white/20 backdrop-blur-lg rounded-full shadow-md transition-colors duration-300 hover:text-yellow-400"
+          >
+            GitHub <GithubIcon />
+          </a>
         </div>
+
       </main>
     </div>
+
   );
 }
