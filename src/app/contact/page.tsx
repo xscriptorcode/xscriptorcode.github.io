4e1a1c010c8c0bea6d3b527d1b3f0947cefff974
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-06-27 10:09:32 +0200

    update contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -15,18 +15,67 @@ export default function Home() {
       <CustomRippleCursor />
       <main className={`${PagesStyles.mainContainer}`}>
         <div className="flex-1 relative p-6 lg:pl-12 shadow-md min-h-[224px] flex flex-col justify-start">
-        <h1 className={`${PagesStyles.titles} ${Roboto_font.className}`}>
-          <DecryptedText
-            text="Contact"
-            animateOn="view"
-            sequential
-            revealDirection="center"
-            speed={90}
-            maxIterations={60}
-            encryptedClassName="text-red-500 animate-pulse"
-            parentClassName="text-2xl tracking-wide"
-          />
-        </h1>
+          <h1 className={`${PagesStyles.titles} ${Roboto_font.className}`}>
+            <DecryptedText
+              text="Contact"
+              animateOn="view"
+              sequential
+              revealDirection="center"
+              speed={90}
+              maxIterations={60}
+              encryptedClassName="text-red-500 animate-pulse"
+              parentClassName="text-2xl tracking-wide"
+            />
+          </h1>
+          <div className={`bg-black/70 border border-gray-700 rounded-xl shadow-lg px-8 py-8 w-full max-w-1/3 mx-auto`}>
+            <div className="flex items-center">
+              <span className={`text-white ${Roboto_font.className}`}> 
+            <DecryptedText
+              text="@Name*://"
+              animateOn="view"
+              sequential
+              revealDirection="center"
+              speed={90}
+              maxIterations={60}
+              encryptedClassName="text-blue-200 animate-pulse"
+              parentClassName="tracking-wide"
+            /> 
+          </span>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="bg-transparent border-0 border-b border-dashed border-gray-500 text-white focus:outline-none w-full"
+              />
+              </div>
+              <div className="flex items-center">
+              <span className="w-24 text-white">E-Mail*:</span>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="bg-transparent border-0 border-b border-dashed border-gray-500 text-white focus:outline-none w-full"
+              />
+              </div>
+              <div className="flex items-center">
+              <span className="w-24 text-white">Number:</span>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="bg-transparent border-0 border-b border-dashed border-gray-500 text-white focus:outline-none w-full"
+              />
+              </div>
+              <div className="flex items-center">
+              <span className="w-24 text-white">Message*:</span>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="bg-transparent border-0 border-b border-dashed border-gray-500 text-white focus:outline-none w-full"
+              />
+              </div>
+          </div>
         </div>
       </main>
     </div>
