ee1bd99c9bf207a18990700b3cda35a2629225e5
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-06-29 18:24:09 +0200

    update styles

--- src/app/components/pagesstyles.ts
+++ src/app/components/pagesstyles.ts
@@ -10,5 +10,16 @@ export const PagesStyles = {
     /*Footer*/
     footerStyles: "text-center py-6 text-sm text-gray-400",
     /* Main */
-    mainContainer: "min-h-screen px-4 py-10"
+    mainContainer: "min-h-screen px-4 py-10",
+
+    /*Pages */
+    /* Home */
+    generalGlassmorphism: "w-full max-w-5xl mx-auto flex flex-col items-center justify-center p-6 sm:p-10 bg-white/1 backdrop-blur-lg border border-white/20 rounded-2xl shadow-md",
+    encryptedClassNameTexts: "text-red-500 animate-pulse",
+    parentClassNameTexts: "text-2xl tracking-wide",
+    smallParentClassName:"text-2xl lg:text-4xl tracking-wide",
+    pdescriptionClassName: "text-sm lg:text-base mt-1 lg:mt-5 text-right",
+    descriptionClassName: "text-white transition-colors duration-300",
+    homeContactButton: "inline-block mt-6 px-5 py-2  bg-white/6 backdrop-blur-lg border border-white/20 rounded-full  text-amber-50 font-medium hover:bg-amber-50 hover:text-black transition",
+    
 };
